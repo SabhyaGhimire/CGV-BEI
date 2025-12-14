f3858313286a02b32b311a19cc87a74fd7a974b8
Suraj <Surajnpfr@users.noreply.github.com>
2025-12-14 09:31:26 +0545

    Clean up duplicate files and folders

--- LAB1/Arun_Sauden/LAB1/1stlab.py
+++
@@ -1,8 +0,0 @@
-nums = []
-
-for i in range(5):
-    number = float(input(f"Enter number {i+1}: "))
-    nums.append(number)
-
-largest = max(nums)
-print("The largest number is:", largest)
--- LAB1/Arun_Sauden/LAB1/Qn2.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Enter a number to print its multiplication table: "))
-print(f"Multiplication table of {num}:")
-
-for i in range(1, 11):
-    print(num, "x", i, "=", num * i)
